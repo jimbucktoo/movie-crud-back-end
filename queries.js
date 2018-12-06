e24f3f186a4db5f2da3d8d040372dfc990eb1098
jimbucktoo <jamesliang.g@gmail.com>
2018-12-06 11:28:05 -0700

    heroku initialization

---
+++ queries.js
@@ -0,0 +1,21 @@
+const db = require('./database-connection')
+module.exports = {
+    getAll(){
+        return db.select().from('movies')
+    },
+    getById(id){
+        return db('movies').where('id', id).first()
+    },
+    createMovie(item){
+        return db('movies').insert(item).returning('*')
+    },
+    updateMovie(id, updateMovie){
+        return db('movies').where("id", id).update(updateMovie).returning('*')
+    },
+    deleteMovie(id){
+        return db('movies').where('id', id).delete()
+    },
+    deleteAll(){
+        return db('movies').delete()
+    }
+}
