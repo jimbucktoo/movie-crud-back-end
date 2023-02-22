5bfa342504a34c70202026b8c9885ca8660c059e
jimbucktoo <jamesliang.g@gmail.com>
2023-02-22 15:55:32 -0600

    critical correction

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 
     development: {
         client: 'pg',
-        connection: 'postgresql://localhost/movie-crud'
+        connection: 'postgresql://localhost/moviecrud'
     },
 
     production: {
