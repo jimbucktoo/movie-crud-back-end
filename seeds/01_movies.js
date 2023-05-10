385d7b0ad6cbc7fdcb6d2367176ad7d454be8767
jimbucktoo <jamesliang.g@gmail.com>
2023-05-10 12:42:28 -0500

    critical seed data correction

--- seeds/01_movies.js
+++ seeds/01_movies.js
@@ -1,8 +1,10 @@
 exports.seed = function(knex, Promise) {
-    return knex('movies').del()
-        .then(function () {
-            return knex('movies').insert([
-                {title: 'This Is The End', directors: 'Seth Rogen, Evan Goldberg', year: 2013, rating: 5, poster_url: 'https://m.media-amazon.com/images/M/MV5BMTQxODE3NjM1Ml5BMl5BanBnXkFtZTcwMzkzNjc4OA@@._V1_.jpg'}
-            ])
-            })
-}
+  return knex('movies').del()
+    .then(function () {
+      return knex('movies').insert([
+        {title: 'Spirited Away', directors: 'Hayao Miyazaki', year: 2002, rating: 5, poster_url: 'https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg'},
+        {title: 'John Wick: Chapter 4', directors: 'Chad Stahelski', year: 2023, rating: 3, poster_url: 'https://assets-prd.ignimgs.com/2023/02/08/jw4-2025x3000-online-character-1sht-keanu-v187-1675886090936.jpg'},
+        {title: 'Godzilla vs. Kong', directors: 'Adam Wingard', year: 2021, rating: 3, poster_url: 'https://m.media-amazon.com/images/M/MV5BMzk2ZmYxNTUtODlhMi00NzE2LTkwMTctYjg0ODQ1ZjkyNzJmXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg'}
+      ]);
+    });
+};
