2138cd7ea39a380f26a4a66fc851def3a2a5e49b
jimbucktoo <jamesliang.g@gmail.com>
2023-12-05 18:03:24 -0600

    critical correction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 5000
 const cors = require('cors')
 const queries = require('./queries')
 const { graphqlHTTP } = require('express-graphql')
